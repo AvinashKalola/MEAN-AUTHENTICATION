8568490dbe919ed0eb659ab20133d946e163d747
Avinash Kalola <avinash_kalola@yahoo.com>
2017-01-23 22:59:10 +0530

    Change facebook authentication call back URL

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '629444930595634',
 		'clientSecret' 	: '1cb63e4a680bc424aed43d9f48ff4895', 
-		'callbackURL' 	: 'http://localhost:3000/login/facebook/callback'
+		'callbackURL' 	: 'https://mean-authentication.herokuapp.com/login/facebook/callback'
 	}
 };
