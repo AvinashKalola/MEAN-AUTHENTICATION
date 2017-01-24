817111679245f121a63dfba5f51d4615a776784c
Avinash Kalola <avinash_kalola@yahoo.com>
2017-01-24 19:41:18 +0530

    Change in deserialization

--- passport/init.js
+++ passport/init.js
@@ -1,16 +1,19 @@
 var facebook = require('../services/facebook');
 var User = require('../models/user');
+var config = require('config.json');
+var mongo = require('mongoskin');
+var db = mongo.db(config.connectionString, { native_parser: true });
+db.bind('users');
 
 module.exports = function(passport){
 
-	
     passport.serializeUser(function(user, done) {
         console.log('serializing user: ');console.log(user);
         done(null, user._id);
     });
 
     passport.deserializeUser(function(id, done) {
-        User.findById(id, function(err, user) {
+       db.users.findById(id, function(err, user) {
             console.log('deserializing user:',user);
             done(err, user);
         });
